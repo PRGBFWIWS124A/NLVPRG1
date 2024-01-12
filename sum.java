c866c07ad5603a7a08aeb03412d7bb841f1e8e56
Niclas-FH <Niclas_Leon.Verbrueggen@edu.fhdw.de>
2024-01-12 12:37:26 +0100

    Echo und sum erstellt

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int one = Integer.parseInt(args[0]);
+        int two = Integer.parseInt(args[1]);
+        int X = one+two;
+        System.out.println(X);
+    }
+}
